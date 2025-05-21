01223945b9baf39e5c94dfc62f2a1b461a52351e
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-05-21 13:21:55 -0400

    Added abilites selector

--- client/src/Card.tsx
+++ client/src/Card.tsx
@@ -1,11 +1,22 @@
 // Import React
 import React from 'react';
 
+import { useState } from 'react';
+
 type PokemonProps = {
   pokemon: {
     name: string;
     height: number;
     weight: number;
+    abilities: {
+      ability: {
+        name: string;
+        url: string;
+      };
+      is_hidden: boolean;
+      slot: number;
+    }[];
+
     sprites: {
       front_default: string;
     };
@@ -13,6 +24,13 @@ type PokemonProps = {
 };
 
 const Card: React.FC<PokemonProps> = ({ pokemon }) => {
+  const [selectedAbility, setSelectedAbility] = useState('');
+
+  const handleAbilityChange = (e) => {
+    setSelectedAbility(e.target.value);
+    return;
+  };
+
   return (
     <div className='border border-gray-400 p-4 rounded shadow-md bg-white w-64'>
       <h2 className='text-xl font-bold capitalize'>{pokemon.name}</h2>
@@ -21,6 +39,13 @@ const Card: React.FC<PokemonProps> = ({ pokemon }) => {
         alt={pokemon.name}
         className='w-32 h-32 mx-auto my-2'
       />
+      <select value={selectedAbility} onChange={handleAbilityChange}>
+        {pokemon.abilities.map((abilitySet) => (
+          <option key={abilitySet.ability.name} value={abilitySet.ability.name}>
+            {abilitySet.ability.name}
+          </option>
+        ))}
+      </select>
       <p>Height: {pokemon.height}</p>
       <p>Weight: {pokemon.weight}</p>
     </div>
